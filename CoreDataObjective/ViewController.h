a1a259b6a509750a546baa668b79fd0a97806af2
SD <sd@client.openvpn.net>
2017-07-17 18:01:40 +0530

    Initial Commit

---
+++ CoreDataObjective/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataObjective
+//
+//  Created by SmartEquip on 7/17/17.
+//  Copyright © 2017 jaisimha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
